5cf647d96a26284a4688753a000d9f152f0330ad
ygtdev <51426246+ygtdev@users.noreply.github.com>
2021-01-04 17:35:34 +0300

    Update cnames_active.js
    ygtdev.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -2852,6 +2852,7 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "zykj.now.sh", // noCF
   "zyy": "zyyou.github.io/notes"
+  "ygtdev": "ygtdev.github.io",
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
